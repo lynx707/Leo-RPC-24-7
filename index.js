dd04be87f512f67f9f22670150e16eef6e7a1e79
Lynx <157897299+lynx707@users.noreply.github.com>
2024-01-31 22:24:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -24,27 +24,27 @@ client.on('ready', async () => {
 
   const r = new Discord.RichPresence()
     .setApplicationId('1079010612769722508')
-    .setType('PLAYING')
+    .setType('WATCHING')
     .setURL('https://www.youtube.com/patkhet') //Must be a youtube video link 
-    .setState('Fiona')
-    .setName('with Kitty')
+    .setState('He is only mine')
+    .setName('My Boy ♡')
     .setDetails(`THE NAME IT SHOWS YOUR STREAMING [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1146452662594842644/1201435643008520243/Snapchat-1522808550.jpg') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Fiona') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/869609600842858526/1201430593251061831/Verify.gif') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Verify') //Text when you hover the Small image
-    .addButton('Discord Server', 'https://discord.gg/cTyrr22e5X')
-    .addButton('Facebook Page', 'https://facebook.com/patkhet.lol');
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1138126775856152626/1139828092781731860/a_6563b456acdc0fd74afbe3eeb945c79b.gif') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('♡♡♡') //Text when you hover the Large image
+    .setAssetsSmallImage('https://cdn.discordapp.com/emojis/730639619153985546.gif') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Heart') //Text when you hover the Small image
+    .addButton('Discord Server', 'https://discord.gg/STrSPVtun3')
+    .addButton('Instagram Page', 'https://instagram.com/patkhet.lol');
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
+  client.user.setPresence({ status: "onilne" }); //dnd, online, idle, offline
 
   let prevTime = null;
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `Meow Meow`;
+      const newDetails = `My Bae 🫦`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
